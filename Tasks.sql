1de3da396f7e014f49c20fe8a46566a53c6271f3
AlekseyDolgov <59308915+AlekseyDolgov@users.noreply.github.com>
2021-04-26 11:50:35 +0300

    Добавление таблицы tasks
    Добавил таблицу tasks. tasks - хранит в себе задачи

--- Tasks.sql
+++ Tasks.sql
@@ -25,3 +25,29 @@ CREATE TABLE projects (
     `description` TEXT,
     complite BOOL
 );
+#Добавил tasks (Долгов Алекскй)
+create table tasks (
+id int auto_increment primary key,
+`name` varchar (32) not null,
+`description` varchar (128),
+type_task_id int,
+compfite bool not null,
+grops_id int,
+objects_id int,
+data_begin datetime,
+data_end datetime, 
+data_add datetime not null,
+data_edit datetime,
+periods_id int,
+parent_id int
+);
+alter table tasks
+add foreign key (type_task_id) references type_task (id);
+alter table tasks
+add foreign key (groups_id) references `groups` (id);
+alter table tasks
+add foreign key (projects_id) references projects (id);
+alter table tasks
+add foreign key (periods_id) references periods (id);
+alter table tasks
+add foreign key (perent_id) references tasks (id);
