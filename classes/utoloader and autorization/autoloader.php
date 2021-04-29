7b634c16ce23f3343341b9e13d752cfd562c5f38
loliDeanon <82580177+loliDeanon@users.noreply.github.com>
2021-04-29 15:01:56 +0300

    new autoloader

--- classes/utoloader and autorization/autoloader.php
+++ classes/utoloader and autorization/autoloader.php
@@ -1,9 +1,29 @@
-<?php
+<?php 
 
-function my_autoloader($class)
-{
-    include $class . ".php";
+function my_autoloader($class) {
+    $directorys = array ('Task_planer/',
+    'Task_planer/planer.log/',
+    'Task_planer/projects/',
+    'Task_planer/projects/public/',
+    'Task_planer/planer.log/controllers/'/
+    'Task_planer/planer.log/models',
+    'Task_planer/planer.log/view',
+    'Task_planer/planer.log/models/autoloader',
+    'Task_planer/planer.log/models/users'
+);
+    if(file_exists($class. '.php')){
+        include $class . ".php";
+        return;
+    }
+    else {
+        foreach($directorys as $directory)
+        {
+            if(file_exists($directory.$class . '.php'))
+            {
+                include ($directory.$class . '.php');
+                return;
+            }   
+        }
+    } 
 }
-
 spl_autoload_register("my_autoloader");
-
