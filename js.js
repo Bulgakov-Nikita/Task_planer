287777f37f8b1865843cc4bbcb59af328c4f84a9
Jensen1488 <82801427+Jensen1488@users.noreply.github.com>
2021-04-30 11:16:50 +0300

    Update js.js

--- js.js
+++ js.js
@@ -9,8 +9,38 @@ popupToggle.onclick = function(){
 popupClose.onclick = function(){
 	popurAdd.style.display = "none";
 }
+////////////////////////////////////////////////
+popurAdd1 = document.getElementById('menuAdd1');
+popupClose1 = document.querySelector('.close1');
+popupToggle1 = document.getElementById('muBth1');
 
+popupToggle1.onclick = function(){
+	popurAdd1.style.display = "inline";
+}
+popupClose1.onclick = function(){
+	popurAdd1.style.display = "none";
+}
 
-//html
-//	   <div id="mg" class="g">
-//	   <span class="close">&times;</span>
+////////////////////////////////////////////////
+popurAdd2 = document.getElementById('menuAdd2');
+popupClose2 = document.querySelector('.close2');
+popupToggle2 = document.getElementById('muBth2');
+
+popupToggle2.onclick = function(){
+	popurAdd2.style.display = "inline";
+}
+popupClose2.onclick = function(){
+	popurAdd2.style.display = "none";
+}
+////////////////////////////////////////////////
+popurAdd3 = document.getElementById('menuAdd3');
+popupClose3 = document.querySelector('.close3');
+popupToggle3 = document.getElementById('muBth3');
+
+popupToggle3.onclick = function(){
+	popurAdd3.style.display = "inline";
+}
+popupClose3.onclick = function(){
+	popurAdd3.style.display = "none";
+}
+////////////////////////////////////////////////
