97bed9e2f46e6f940ec6822467753dc65f2d9d38
MariiSh <83400577+MariiSh@users.noreply.github.com>
2021-04-30 14:02:30 +0300

    123--------------------
    Добавление кнопок

---
+++ code/js.js
@@ -0,0 +1,116 @@
+let g =document.getElementById('mg');
+popupToggle = document.getElementById('muBth');
+popupClose = document.querySelector('.close');
+popurAdd = document.getElementById('menuAdd');
+popupToggle.onclick = function(){
+	if (popurAdd.style.display == "inline"){
+		popurAdd.style.display = "none";
+	}
+	else {
+	popurAdd.style.display = "inline";
+	}
+}
+popupClose.onclick = function(){
+	popurAdd.style.display = "none";
+}
+////////////////////////////////////////////////
+popurAdd1 = document.getElementById('menuAdd1');
+popupClose1 = document.querySelector('.close1');
+popupToggle1 = document.getElementById('muBth1');
+
+popupToggle1.onclick = function(){
+	if (popurAdd1.style.display == "inline"){
+		popurAdd1.style.display = "none";
+	}
+	else {
+	popurAdd1.style.display = "inline";
+	}
+}
+popupClose1.onclick = function(){
+	popurAdd1.style.display = "none";
+}
+
+////////////////////////////////////////////////
+popurAdd2 = document.getElementById('menuAdd2');
+popupClose2 = document.querySelector('.close2');
+popupToggle2 = document.getElementById('muBth2');
+
+popupToggle2.onclick = function(){
+	if (popurAdd2.style.display == "inline"){
+		popurAdd2.style.display = "none";
+	}
+	else {
+	popurAdd2.style.display = "inline";
+	}
+}
+popupClose2.onclick = function(){
+	popurAdd2.style.display = "none";
+}
+////////////////////////////////////////////////
+popurAdd4 = document.getElementById('menuAdd4');
+popupClose4 = document.querySelector('.close4');
+popupToggle4 = document.getElementById('muBth4');
+
+popupToggle4.onclick = function(){
+	if (popurAdd4.style.display == "inline"){
+		popurAdd4.style.display = "none";
+	}
+	else {
+	popurAdd4.style.display = "inline";
+	}
+}
+popupClose4.onclick = function(){
+	popurAdd4.style.display = "none";
+}
+
+
+
+
+
+////////////////////////////////////////////////
+popurAdd3 = document.getElementById('menuAdd3');
+popupClose3 = document.querySelector('.close3');
+popupToggle3 = document.getElementById('muBth3');
+popupToggle3.onclick = function(){
+	if (popurAdd3.style.display == "inline"){
+		popurAdd3.style.display = "none";
+	}
+	else {
+	popurAdd3.style.display = "inline";
+	}
+}
+popupClose3.onclick = function(){
+	popurAdd3.style.display = "none";
+}
+////////////////////////////////////////////////
+popurAdd5 = document.getElementById('menuAdd5');
+popupClose5 = document.querySelector('.close5');
+popupToggle5 = document.getElementById('muBth5');
+
+popupToggle5.onclick = function(){
+	if (popurAdd5.style.display == "inline"){
+		popurAdd5.style.display = "none";
+	}
+	else {
+	popurAdd5.style.display = "inline";
+	}
+}
+popupClose5.onclick = function(){
+	popurAdd5.style.display = "none";
+}
+////////////////////////////////////////////////
+popurAdd6 = document.getElementById('menuAdd6');
+popupClose6 = document.querySelector('.close6');
+popupToggle6 = document.getElementById('muBth6');
+
+popupToggle6.onclick = function(){
+	if (popurAdd6.style.display == "inline"){
+		popurAdd6.style.display = "none";
+	}
+	else {
+	popurAdd6.style.display = "inline";
+	}
+}
+popupClose6.onclick = function(){
+	popurAdd6.style.display = "none";
+}
