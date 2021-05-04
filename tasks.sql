f835d7b4a6ba2d6d2a9b27a660f9ec57052c9158
XxxdimooonxxX <63523101+XxxdimooonxxX@users.noreply.github.com>
2021-05-04 14:31:31 +0300

    Обновление диаграммы и создание БД

--- tasks.sql
+++ tasks.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE tasks;
-USE tasks;
+CREATE DATABASE tasks_db;
+USE tasks_db;
 
 CREATE TABLE type_task (
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -11,15 +11,21 @@ CREATE TABLE type_interval (
    	`name` VARCHAR(64) NOT NULL
 );
 
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NOT NULL
+);
+
 CREATE TABLE projects (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	`name` VARCHAR(64) NOT NULL,
 	date_begin DATETIME,
 	date_end DATETIME,
 	`description` TEXT,
-	complite BOOL,
-    created_at INT,
-    created_by INT,
+    created_at INT NOT NULL,
+    created_by INT NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE,
     updated_at INT,
     updated_by INT,
     deleted_at INT,
@@ -34,11 +40,11 @@ CREATE TABLE `groups` (
 	date_begin DATETIME,
 	date_end DATETIME,
 	`description` TEXT,
-	complite BOOL,
 	projects_id INT,
-	FOREIGN KEY (projects_id) REFERENCES projects(id),
-    created_at INT,
-    created_by INT,
+	FOREIGN KEY (projects_id) REFERENCES projects(id) ON DELETE CASCADE,
+    created_at INT NOT NULL,
+    created_by INT NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE,
     updated_at INT,
     updated_by INT,
     deleted_at INT,
@@ -52,33 +58,33 @@ CREATE TABLE tasks (
 	`name` VARCHAR(64) NOT NULL,
 	`description` TEXT,
 	type_task_id INT,
-	complite BOOL NOT NULL,
 	groups_id INT,
 	projects_id INT,
 	data_begin DATETIME,
 	data_end DATETIME, 
 	parent_id INT, 
-	FOREIGN KEY (type_task_id) REFERENCES type_task(id),
-	FOREIGN KEY (groups_id) REFERENCES `groups`(id),
-	FOREIGN KEY (projects_id) REFERENCES projects(id),
-	FOREIGN KEY (parent_id) REFERENCES tasks(id),
-    created_at INT,
-    created_by INT,
+	FOREIGN KEY (type_task_id) REFERENCES type_task(id) ON DELETE CASCADE,
+	FOREIGN KEY (groups_id) REFERENCES `groups`(id) ON DELETE CASCADE,
+	FOREIGN KEY (projects_id) REFERENCES projects(id) ON DELETE CASCADE,
+	FOREIGN KEY (parent_id) REFERENCES tasks(id) ON DELETE CASCADE,
+    created_at INT NOT NULL,
+    created_by INT NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE,
     updated_at INT,
     updated_by INT,
     deleted_at INT,
     deleted_by INT,
-    `active` TINYINT(1),
+    `active` TINYINT(1) NOT NULL,
     `lock` INT
 );
 
 CREATE TABLE periods (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	tasks_id INT,
-	`interval` INT,
-	type_interval_id INT,
-	FOREIGN KEY (type_interval_id) REFERENCES type_interval(id),
-	FOREIGN KEY (tasks_id) REFERENCES tasks(id)
+	tasks_id INT NOT NULL,
+	`interval` INT NOT NULL,
+	type_interval_id INT NOT NULL,
+	FOREIGN KEY (type_interval_id) REFERENCES type_interval(id) ON DELETE CASCADE,
+	FOREIGN KEY (tasks_id) REFERENCES tasks(id) ON DELETE CASCADE
 );
 
 #Подумать ещё
@@ -94,6 +100,11 @@ CREATE TABLE task_action (
 	tasks_id INT,
 	action_id INT,
 	sort INT,
-	FOREIGN KEY (tasks_id) REFERENCES tasks(id),
-	FOREIGN KEY  (action_id) REFERENCES `action`(id)
+	FOREIGN KEY (tasks_id) REFERENCES tasks(id) ON DELETE CASCADE,
+	FOREIGN KEY  (action_id) REFERENCES `action`(id) ON DELETE CASCADE
 );
+
+INSERT INTO  type_task(id, `name`) values
+	(null, 'Лёгкая'),
+	(null, 'Средняя'),
+	(null, 'Сложная');
