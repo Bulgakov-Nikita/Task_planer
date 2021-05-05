939d673e14ae19a04da09eb1d35e23820ab283bc
Sasha <sanya.ryzanov2015@yandex.ru>
2021-05-05 22:03:26 +0300

    Добавлен класс c_setting в контроллере(требуется доработка) и добавлен файл для редактирования аккаунта(требуется доработка)

---
+++ planer.log/views/setting.php
@@ -0,0 +1,11 @@
+<h1>Редактирование аккаунта</h1>
+<form action="/setting/setting" method="post">
+    Логин:
+    <input type="text" name="login">
+    <br/>
+    Пароль:
+    <input type="password" name="password">
+    <br/>
+    <button name="type_bt" value="users">set</button>
+</form>
+
