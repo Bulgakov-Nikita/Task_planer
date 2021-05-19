02a8756b1b9a2f0050a94ec5e279beaffa9ea854
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-19 16:19:07 +0300

    таблица
    таблица с утверждающими, с подписантами, реквизиты

---
+++ migrations/m190521_142000_creat_utverzhdauchiy_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Migration;
+
+class m190521_142200_creat_utverzhdauchiy_table extends Migration{
+    public function safeUp(){
+        $this->createTable('utverzhdauchiy', [
+            'id' => $this->primaryKey()->notNull()->comment('Первичный ключ'),
+            'name' => $this->char(60)->notNull()->comment('Реквизиты/ФИО'),
+        ]);
+        $this->addCommentOnTable('utverzhdauchiy', 'для хранения реквизитов утверждающего');
+    }
+    public function safeDown()
+    {
+        $this->dropTable('utverzhdauchiy');
+
+    }
+
+}
