05a390dfbfac9d2e5c963cfab6dd633802bef4a8
Gasanov-Elvin <82573346+Gasanov-Elvin@users.noreply.github.com>
2021-05-19 16:18:39 +0300

    Add files via upload

---
+++ migrations/m190521_142300_create_sroc_obucheniya_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m190521_142300_create_sroc_obucheniya extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('sroc_obucheniya', [
+            'id' => $this->primaryKey()->notNull()->comment('Первичный ключ'),
+            'sroc' => $this->char(45)->notNull()->comment('Срок')
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('sroc_obucheniya');
+    }
+}
