95542b3d3af72d423fc4d26a9efb0a37ead98949
Булгаков Никита <77019664+Bulgakov-Nikita@users.noreply.github.com>
2021-05-21 15:36:00 +0300

    Add files via upload

---
+++ backend/Parser/controllers/ImportController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\modules\admin\Parser\controllers;
+
+use yii\web\Controller;
+
+class ImportController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('test_view');
+    }
+}
---
+++ backend/Parser/views/import/test_view.php
@@ -0,0 +1,5 @@
+<div class="parser-default-index">
+    <h1>
+        Test controller
+    </h1>
+</div>
