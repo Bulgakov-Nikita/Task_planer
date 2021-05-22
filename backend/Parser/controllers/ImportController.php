949bb6525e2a4e7ea7811acf8eec88aa765a94cb
Булгаков Никита <77019664+Bulgakov-Nikita@users.noreply.github.com>
2021-05-22 09:56:21 +0300

    Add files via upload

--- backend/Parser/controllers/ImportController.php
+++ backend/Parser/controllers/ImportController.php
@@ -2,12 +2,22 @@
 
 namespace app\modules\admin\Parser\controllers;
 
+use Yii;
 use yii\web\Controller;
+use app\modules\admin\Parser\models\Parser;
+use yii\web\UploadedFile;
 
 class ImportController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('test_view');
+        $model = new Parser();
+        if (Yii::$app->request->isPost) {
+            $model->file = UploadedFile::getInstance($model, 'file');
+            $model->parse();
+        } else {
+            // если файл не выбран
+            return $this->render('import_file', ['model' => $model]);
+        }
     }
 }
