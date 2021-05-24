a424d7b70e34927a1e59343bcfd5343a7eca089c
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-24 11:10:57 +0300

    Изменение
    Убран notNull() в делет и убран внешний ключ type_comp_id

--- migrations2/m210521_193700_create_comp_table.php
+++ migrations2/m210521_193700_create_comp_table.php
@@ -10,29 +10,20 @@ class m210521_193700_create_comp_table extends Migration
             'id' => $this->primaryKey()->notNull()->comment('Первичный ключ'),
             'index' => $this->string(45)->notNull()->comment('индекс'),
             'soderzhanie' => $this->text()->notNull()->comment('содержание'),
-            'type_comp_id' => $this->integer(11)->notNull()->comment('ссылка на план'),
             'create_at' => $this->integer(11)->notNull()->comment('дата создания'),
             'create_by' => $this->integer(11)->notNull()->comment('кем создано'),
             'update_at' => $this->integer(11)->notNull()->comment('дата обновления'),
             'update_by' => $this->integer(11)->notNull()->comment('кем создано'),
-            'delete_at' => $this->integer(11)->notNull()->comment('дата удаления'),
-            'delete_by' => $this->integer(11)->notNull()->comment('кем удалено'),
+            'delete_at' => $this->integer(11)->comment('дата удаления'),
+            'delete_by' => $this->integer(11)->comment('кем удалено'),
             'active' => $this->tinyInteger(1)->notNull()->comment('статус'),
             'lock' => $this->integer(11)->notNull()->comment('блокировка')
         ]);
         $this->addCommentOnTable('comp', 'Таблица для хранения информации о компетенции');
-
-        $this->addForeignKey(
-            'FK_type_comp_id',
-            'comp',
-            'type_comp_id',
-            'type_comp',
-            'id');
     }
 
     public function safeDown()
     {
         $this->dropTable('comp');
-        $this->dropForeignKey('FK_type_comp_id');
     }
 }
