5429baecf5c1a32cce07c3bf4a631cc3728d52fd
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-24 10:44:34 +0300

    Изменение
    Убран notNull() в делет и перент id

--- migrations2/m200521_212300_create_prof_standart_table.php
+++ migrations2/m200521_212300_create_prof_standart_table.php
@@ -12,13 +12,13 @@ class m200521_212300_create_prof_standart_table extends Migration
             'name' => $this->integer(11)->notNull()->comment('Название проф стандарта'),
             'date' => $this->date()->notNull()->comment('дата проф стандарта'),
             'number' => $this->string(45)->notNull()->comment('номер проф стандарта'),
-            'parent_id' => $this->integer(11)->notNull()->comment('ссылка самого на себя'),
+            'parent_id' => $this->integer(11)->comment('ссылка самого на себя'),
             'create_at' => $this->integer(11)->notNull()->comment('дата создания'),
             'create_by' => $this->integer(11)->notNull()->comment('кем создано'),
             'update_at' => $this->integer(11)->notNull()->comment('дата обновления'),
             'update_by' => $this->integer(11)->notNull()->comment('кем создано'),
-            'delete_at' => $this->integer(11)->notNull()->comment('дата удаления'),
-            'delete_by' => $this->integer(11)->notNull()->comment('кем удалено'),
+            'delete_at' => $this->integer(11)->comment('дата удаления'),
+            'delete_by' => $this->integer(11)->comment('кем удалено'),
             'active' => $this->tinyInteger(1)->notNull()->comment('статус'),
             'lock' => $this->integer(11)->notNull()->comment('блокировка')
         ]);
