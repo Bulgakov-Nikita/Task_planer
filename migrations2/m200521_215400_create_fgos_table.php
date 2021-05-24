130cad2fc708f392186072b6fa9f8935320c4895
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-24 11:20:06 +0300

    Изменение
    Убран notNull() в делет

--- migrations2/m200521_215400_create_fgos_table.php
+++ migrations2/m200521_215400_create_fgos_table.php
@@ -13,8 +13,8 @@ class m200521_215400_create_fgos_table extends Migration{
             'create_by' => $this->integer(11)->notNull()->comment('кем создано'),
             'update_at' => $this->integer(11)->notNull()->comment('дата обновления'),
             'update_by' => $this->integer(11)->notNull()->comment('кем создано'),
-            'delete_at' => $this->integer(11)->notNull()->comment('дата удаления'),
-            'delete_by' => $this->integer(11)->notNull()->comment('кем удалено'),
+            'delete_at' => $this->integer(11)->comment('дата удаления'),
+            'delete_by' => $this->integer(11)->comment('кем удалено'),
             'active' => $this->tinyInteger(1)->notNull()->comment('статус'),
             'lock' => $this->integer(11)->notNull()->comment('блокировка')
         ]);
