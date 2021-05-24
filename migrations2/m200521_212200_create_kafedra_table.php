f13a51271e3237aaf5ab121cfb7114de0e12d050
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-24 10:43:11 +0300

    Изменение
    Убран notNull() в делет

--- migrations2/m200521_212200_create_kafedra_table.php
+++ migrations2/m200521_212200_create_kafedra_table.php
@@ -14,8 +14,8 @@ class m200521_212200_create_kafedra_table extends Migration
             'create_by' => $this->integer(11)->notNull()->comment('кем создано'),
             'update_at' => $this->integer(11)->notNull()->comment('дата обновления'),
             'update_by' => $this->integer(11)->notNull()->comment('кем создано'),
-            'delete_at' => $this->integer(11)->notNull()->comment('дата удаления'),
-            'delete_by' => $this->integer(11)->notNull()->comment('кем удалено'),
+            'delete_at' => $this->integer(11)->comment('дата удаления'),
+            'delete_by' => $this->integer(11)->comment('кем удалено'),
             'active' => $this->tinyInteger(1)->notNull()->comment('статус'),
             'lock' => $this->integer(11)->notNull()->comment('блокировка')
         ]);
