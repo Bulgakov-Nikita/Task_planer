d67d58b26495644f382f4678d1b33fb8c4861517
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-25 09:20:36 +0300

    Миграция
    Перезалив

---
+++ migrations/m240521_215500_create_sprav_dis_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use \yii\db\Migration;
+
+class m240521_215500_create_sprav_dis_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('sprav_dis', [
+            'id' => $this->primaryKey()->notNull()->comment('Первичный ключ'),
+            'name' => $this->string(45)->notNull()->comment('тип дисциплины'),
+            'kafedra_id' => $this->integer()->comment('Ссылка на кафедру'),
+
+            'create_at' => $this->integer(11)->notNull()->comment('дата создания'),
+            'create_by' => $this->integer(11)->notNull()->comment('кем создано'),
+            'update_at' => $this->integer(11)->notNull()->comment('дата обновления'),
+            'update_by' => $this->integer(11)->notNull()->comment('кем создано'),
+            'delete_at' => $this->integer(11)->comment('дата удаления'),
+            'delete_by' => $this->integer(11)->comment('кем удалено'),
+            'active' => $this->tinyInteger(1)->notNull()->comment('статус'),
+            'lock' => $this->integer(11)->notNull()->comment('блокировка')
+        ]);
+        $this->addCommentOnTable('sprav_dis', 'Таблица для хранения информации о типе дисциплины');
+        
+        //FK
+        $this->addForeignKey(
+            'FK_kafedra_id_sprav_dis_id',
+            'sprav_dis',
+            'kafedra_id',
+            'kafedra',
+            'id'
+        );
+        $this->addForeignKey(
+            'FK_c_sprav_dis_id',
+            'sprav_dis',
+            'create_by',
+            'user',
+            'id'
+        );
+        $this->addForeignKey(
+            'FK_u_sprav_dis_id',
+            'sprav_dis',
+            'update_by',
+            'user',
+            'id'
+        );
+        $this->addForeignKey(
+            'FK_d_sprav_dis_id',
+            'sprav_dis',
+            'delete_by',
+            'user',
+            'id'
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('sprav_dis');
+
+        //FK
+        $this->dropForeigenKey('FK_kafedra_id_sprav_dis_id', 'kafedra');
+    }
+}
---
+++ migrations/m240521_220500_create_dc_table.php
@@ -0,0 +1,71 @@
+<?php
+
+use \yii\db\Migration;
+
+class m240521_220500_create_dc_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('dc', [
+            'id' => $this->primaryKey()->notNull()->comment('Первичный ключ'),
+            'comp_id' => $this->integer()->notNull()->comment('ссылка на компетенции'),
+            'disciplins_id' => $this->integer()->notNull()->comment('ссылка на компетенции 2'),
+
+            'create_at' => $this->integer(11)->notNull()->comment('дата создания'),
+            'create_by' => $this->integer(11)->notNull()->comment('кем создано'),
+            'update_at' => $this->integer(11)->notNull()->comment('дата обновления'),
+            'update_by' => $this->integer(11)->notNull()->comment('кем создано'),
+            'delete_at' => $this->integer(11)->comment('дата удаления'),
+            'delete_by' => $this->integer(11)->comment('кем удалено'),
+            'active' => $this->tinyInteger(1)->notNull()->comment('статус'),
+            'lock' => $this->integer(11)->notNull()->comment('блокировка')
+        ]);
+        $this->addCommentOnTable('dc', 'Таблица для хранения информации о компетенции и компетенции 2');
+
+        //FK
+        $this->addForeignKey(
+            'FK_comp_id_cc2_id',
+            'dc',
+            'comp_id',
+            'comp',
+            'id'
+        );
+        $this->addForeignKey(
+            'FK_comp2_id_cc2_id',
+            'dc',
+            'disciplins_id',
+            'disciplins',
+            'id'
+        );
+        $this->addForeignKey(
+            'FK_c_dc_id',
+            'dc',
+            'create_by',
+            'user',
+            'id'
+        );
+        $this->addForeignKey(
+            'FK_u_dc_id',
+            'dc',
+            'update_by',
+            'user',
+            'id'
+        );
+        $this->addForeignKey(
+            'FK_d_dc_id',
+            'dc',
+            'delete_by',
+            'user',
+            'id'
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('dc');
+
+        //FK
+        $this->dropForeigenKey('FK_comp_id_cc2_id', 'comp');
+        $this->dropForeigenKey('FK_comp2_id_cc2_id', 'disciplins');
+    }
+}
