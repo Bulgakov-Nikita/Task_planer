e5917ea19d48415f078acb5b52480fe989996615
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-26 12:52:31 +0300

    Изменения fo
    Изменения количества символов в варчаре в fo

--- migrations/m240521_202000_create_fo_table.php
+++ migrations/m240521_202000_create_fo_table.php
@@ -6,7 +6,7 @@ class m240521_202000_create_fo_table extends Migration{
     public function safeUp(){
         $this->createTable('fo',[
             'id' => $this->primaryKey()->notNull()->comment('Первичный ключ'),
-            'name' => $this->string(10)->notNull()->comment('(очная, заочная) названи формы обучения'),
+            'name' => $this->string(45)->notNull()->comment('(очная, заочная) названи формы обучения'),
             'create_at' => $this->integer(11)->notNull()->comment('дата создания'),
             'create_by' => $this->integer(11)->notNull()->comment('кем создано'),
             'update_at' => $this->integer(11)->notNull()->comment('дата обновления'),
