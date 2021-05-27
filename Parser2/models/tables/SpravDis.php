9885d765bb9ca40d4f49e3ad82a95867e1b63c7a
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-27 22:01:19 +0300

    Парсинг
    Добавлен парс таблиц тип формы, справочник учебного года, дисциплина компетенция доработаны таблицы дисциплин и кафедра компетенций

---
+++ Parser2/models/tables/SpravDis.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace app\modules\admin\Parser\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "sprav_dis".
+ *
+ * @property int $id Первичный ключ
+ * @property string $name тип дисциплины
+ * @property int|null $sprav_kafedra_id Ссылка на кафедру
+ * @property int $create_at дата создания
+ * @property int $create_by кем создано
+ * @property int $update_at дата обновления
+ * @property int $update_by кем создано
+ * @property int|null $delete_at дата удаления
+ * @property int|null $delete_by кем удалено
+ * @property int $active статус
+ * @property int $lock блокировка
+ *
+ * @property Disciplins[] $disciplins
+ * @property User $createBy
+ * @property User $deleteBy
+ * @property SpravKafedra $spravKafedra
+ * @property User $updateBy
+ */
+class SpravDis extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'sprav_dis';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'sprav_kafedra_id' => 'Sprav Kafedra ID',
+            'create_at' => 'Create At',
+            'create_by' => 'Create By',
+            'update_at' => 'Update At',
+            'update_by' => 'Update By',
+            'delete_at' => 'Delete At',
+            'delete_by' => 'Delete By',
+            'active' => 'Active',
+            'lock' => 'Lock',
+        ];
+    }
+
+    /**
+     * Gets query for [[Disciplins]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDisciplins()
+    {
+        return $this->hasMany(Disciplins::className(), ['sprav_dis_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreateBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreateBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'create_by']);
+    }
+
+    /**
+     * Gets query for [[DeleteBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDeleteBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'delete_by']);
+    }
+
+    /**
+     * Gets query for [[SpravKafedra]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSpravKafedra()
+    {
+        return $this->hasOne(SpravKafedra::className(), ['id' => 'sprav_kafedra_id']);
+    }
+
+    /**
+     * Gets query for [[UpdateBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUpdateBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'update_by']);
+    }
+}
