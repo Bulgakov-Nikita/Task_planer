a54e8048b3bcbee23b5bd68d36eb07b698a41a87
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-30 13:35:14 +0300

    Парсинг

---
+++ Parser2/models/tables/TypePeriods.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace app\modules\admin\Parser\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "type_periods".
+ *
+ * @property int $id Первичный ключ
+ * @property string $name (очная, заочная) названи формы обучения
+ * @property int $created_at дата создания
+ * @property int $created_by кем создано
+ * @property int $updated_at дата обновления
+ * @property int $updated_by кем создано
+ * @property int|null $deleted_at дата удаления
+ * @property int|null $deleted_by кем удалено
+ * @property int $active статус
+ * @property int $lock блокировка
+ *
+ * @property Period[] $periods
+ * @property User $createBy
+ * @property User $deleteBy
+ * @property User $updateBy
+ */
+class TypePeriods extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'type_periods';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'created_at' => 'Create At',
+            'created_by' => 'Create By',
+            'updated_at' => 'Update At',
+            'updated_by' => 'Update By',
+            'deleted_at' => 'Delete At',
+            'deleted_by' => 'Delete By',
+            'active' => 'Active',
+            'lock' => 'Lock',
+        ];
+    }
+
+    /**
+     * Gets query for [[Periods]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPeriods()
+    {
+        return $this->hasMany(Period::className(), ['type_periods_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreateBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreateBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[DeleteBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDeleteBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'deleted_by']);
+    }
+
+    /**
+     * Gets query for [[UpdateBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUpdateBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+    }
+}
