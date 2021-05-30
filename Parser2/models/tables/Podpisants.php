cee2093a452d23c7c8d70e870c059c61342c3d4a
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-05-30 12:37:51 +0300

    Парсинг

---
+++ Parser2/models/tables/Podpisants.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace app\modules\admin\Parser\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "podpisants".
+ *
+ * @property int $id PK
+ * @property int $staff_id ссылка на сотрудников
+ * @property int $main_plan_id ссылка на главную таблицу
+ * @property int $created_at дата создания
+ * @property int $created_by кем создано
+ * @property int $updated_at дата обновления
+ * @property int $updated_by кем создано
+ * @property int|null $deleted_at дата удаления
+ * @property int|null $deleted_by кем удалено
+ * @property int $active статус
+ * @property int $lock блокировка
+ *
+ * @property User $createdBy
+ * @property User $deletedBy
+ * @property MainPlan $mainPlan
+ * @property Staff $staff
+ * @property User $updatedBy
+ */
+class Podpisants extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'podpisants';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'staff_id' => 'Staff ID',
+            'main_plan_id' => 'Main Plan ID',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+            'deleted_at' => 'Deleted At',
+            'deleted_by' => 'Deleted By',
+            'active' => 'Active',
+            'lock' => 'Lock',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[DeletedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDeletedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'deleted_by']);
+    }
+
+    /**
+     * Gets query for [[MainPlan]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMainPlan()
+    {
+        return $this->hasOne(MainPlan::className(), ['id' => 'main_plan_id']);
+    }
+
+    /**
+     * Gets query for [[Staff]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStaff()
+    {
+        return $this->hasOne(Staff::className(), ['id' => 'staff_id']);
+    }
+
+    /**
+     * Gets query for [[UpdatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+    }
+}
---
+++ Parser2/models/tables/TypeSession.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace app\modules\admin\Parser\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "type_session".
+ *
+ * @property int $id Первичный ключ
+ * @property string $name Тип сессии
+ * @property int $created_at дата создания
+ * @property int $created_by кем создано
+ * @property int $updated_at дата обновления
+ * @property int $updated_by кем создано
+ * @property int|null $deleted_at дата удаления
+ * @property int|null $deleted_by кем удалено
+ * @property int $active статус
+ * @property int $lock блокировка
+ *
+ * @property Session[] $sessions
+ * @property User $createBy
+ * @property User $deleteBy
+ * @property User $updateBy
+ */
+class TypeSession extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'type_session';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'created_at' => 'Create At',
+            'created_by' => 'Create By',
+            'updated_at' => 'Update At',
+            'updated_by' => 'Update By',
+            'deleted_at' => 'Delete At',
+            'deleted_by' => 'Delete By',
+            'active' => 'Active',
+            'lock' => 'Lock',
+        ];
+    }
+
+    /**
+     * Gets query for [[Sessions]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSessions()
+    {
+        return $this->hasMany(Session::className(), ['type_session_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreateBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreateBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[DeleteBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDeleteBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'deleted_by']);
+    }
+
+    /**
+     * Gets query for [[UpdateBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUpdateBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+    }
+}
