cb1d4b06ac1321f7ebc8a13514a27e08141210df
Alexander-classik <82573292+Alexander-classik@users.noreply.github.com>
2021-06-01 10:55:06 +0300

    Парс

---
+++ Parser2/models/tables/SrocEducation.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace app\modules\admin\Parser\models\tables;
+
+use Yii;
+
+/**
+ * This is the model class for table "sroc_education".
+ *
+ * @property int $id Первичный ключ
+ * @property string $name срок обучения
+ * @property int $created_at дата создания
+ * @property int $created_by кем создано
+ * @property int $updated_at дата обновления
+ * @property int $updated_by кем создано
+ * @property int|null $deleted_at дата удаления
+ * @property int|null $deleted_by кем удалено
+ * @property int $active статус
+ * @property int $lock блокировка
+ *
+ * @property MainPlan[] $mainPlans
+ * @property User $createdBy
+ * @property User $deletedBy
+ * @property User $updatedBy
+ */
+class SrocEducation extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'sroc_education';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+            'deleted_at' => 'Deleted At',
+            'deleted_by' => 'Deleted By',
+            'active' => 'Active',
+            'lock' => 'Lock',
+        ];
+    }
+
+    /**
+     * Gets query for [[MainPlans]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMainPlans()
+    {
+        return $this->hasMany(MainPlan::className(), ['sroc_education_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[DeletedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDeletedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'deleted_by']);
+    }
+
+    /**
+     * Gets query for [[UpdatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'updated_by']);
+    }
+}
